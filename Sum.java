cfd1bb057b49ce5abccc7b63632e01f393da0d91
Vrutik Parmar <vrutikprmr@gmail.com>
2022-01-08 11:06:48 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args){
         int a=5;
         int b=3;
-        System.out.println(a+b);
+        System.out.println("The Sum is " + (a+b));
     }
 }
